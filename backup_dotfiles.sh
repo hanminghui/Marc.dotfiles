42c81fac913211ae872aa39012be02e0051b787c
hanminghui <imhanminghui@gmail.com>
2018-03-18 19:10:57 +0800

    add backup_dotfiles.sh

---
+++ backup_dotfiles.sh
@@ -0,0 +1,5 @@
+# Backup config files
+cp ~/.vimrc ~/Marc.dotfiles/.vimrc;
+cp ~/.Xresources ~/Marc.dotfiles/.Xresources;
+cp ~/.gitconfig ~/Marc.dotfiles/.gitconfig;
+cp ~/.config/i3/config ~/Marc.dotfiles/i3.config;
