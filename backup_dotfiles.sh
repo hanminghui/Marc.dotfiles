222f3556e97510e16c1219f349a6e35306dcb789
hanminghui <imhanminghui@gmail.com>
2019-04-13 11:44:08 +0800

    add tmux.conf

--- backup_dotfiles.sh
+++ backup_dotfiles.sh
@@ -1,8 +1,9 @@
 # Backup config files
-cp ~/.vimrc ~/Marc.dotfiles/.vimrc;
-cp ~/.Xresources ~/Marc.dotfiles/.Xresources;
-cp ~/.gitconfig ~/Marc.dotfiles/.gitconfig;
-cp ~/.config/i3/config ~/Marc.dotfiles/i3.config;
-cp ~/.zprofile ~/Marc.dotfiles/.zprofile
-cp ~/.zshrc ~/Marc.dotfiles/.zshrc
-cp ~/bin/ssr ~/Marc.dotfiles/ssr 
+cp ~/.vimrc ./.vimrc;
+cp ~/.tmux.conf ./.tmux.conf;
+cp ~/.config/i3/config ./i3.config;
+cp ~/.Xresources ./.Xresources;
+cp ~/.gitconfig ./.gitconfig;
+cp ~/.zprofile ./.zprofile
+cp ~/.zshrc ./.zshrc
+cp ~/bin/ssr ./ssr 
