7d32ec389ccc59ae46063318575a620dc99c2241
hanminghui <imhanminghui@gmail.com>
2019-05-16 14:15:49 +0800

    update backup script, only copy when needed

--- backup_dotfiles.sh
+++ backup_dotfiles.sh
@@ -2,6 +2,8 @@
 
 # Backup config files
 
+destination=$PWD
+
 files=()
 # vim
 files+=($HOME/.vimrc)
@@ -20,28 +22,71 @@ files+=($HOME/.zprofile)
 # some scripts
 files+=($HOME/scripts)
 
-echo ==================================================
+echo ================================================================================
 echo Start backup
-echo --------------------------------------------------
+echo --------------------------------------------------------------------------------
 count=0
-for i in ${files[@]}
+update=0
+for old in ${files[@]}
 do
-	if [[ -d $i ]]
-	then
-		count=$((count+1))
-		cp -r $i ./
-		echo "Successfully backup directory: $i"
-	elif [[ -f $i ]]
-	then
-		count=$((count+1))
-		cp $i ./
-		echo "Successfully backup file     : $i"
-	else
-		echo  -e "\033[38;5;202mNo such file or directory    : ${i}\033[0m"
+new="${old##*/}"
+info=" INFO "
+if [[ -e $old ]]
+then # old exist
+	if [[ -e $new ]]
+	then # new exist, check need to update
+		if diff -r "$old" "$new" > /dev/null
+		then # do nothing
+			info=" KEEP "
+			if [[ -d $old ]]
+			then
+				count=$((count+1))
+				echo -e "Successfully backup directory \033[1m[$info]\033[0m: $old"
+			elif [[ -f $old ]]
+			then
+				count=$((count+1))
+				echo -e "Successfully backup file      \033[1m[$info]\033[0m: $old"
+			fi
+		else # update
+			info="UPDATE"
+			if [[ -d $old ]]
+			then
+				count=$((count+1))
+				update=$((update+1))
+				cp -r $old $destination
+				echo -e "Successfully backup directory \033[1;38;5;48m[$info]\033[0m: $old"
+			elif [[ -f $old ]]
+			then
+				count=$((count+1))
+				update=$((update+1))
+				cp $old $destination
+				echo -e "Successfully backup file      \033[1;38;5;48m[$info]\033[0m: $old"
+			fi
+		fi
+	else # new not exist, create
+		info="CREATE"
+		if [[ -d $old ]]
+		then
+			count=$((count+1))
+			update=$((update+1))
+			cp -r $old $destination
+			echo -e "Successfully backup directory \033[1;38;5;226m[$info]\033[0m: $old"
+		elif [[ -f $old ]]
+		then
+			count=$((count+1))
+			update=$((update+1))
+			cp $old $destination
+			echo -e "Successfully backup file      \033[1;38;5;226m[$info]\033[0m: $old"
+		fi
 	fi
+else # old not exist
+	info=" NULL "
+	echo -e "No such file or directory     \033[1;38;5;196m[$info]\033[0m: ${old}"
+fi
 done
-echo --------------------------------------------------
+echo --------------------------------------------------------------------------------
 all=${#files[@]}
 fail=$(($all-$count))
-echo Success: $count, Fail: $fail, in $all files
-echo ==================================================
+keep=$(($count-$update))
+echo Success: $count, Update: $update, Keep: $keep, Fail: $fail, in $all files
+echo ================================================================================
